833d1e8ca09881011988fa114b7aca06a5fbb203
warnerb47 <warnerbang@gmail.com>
2021-11-05 23:39:22 +0000

    remove mongo url from repo

--- soapService/src/main/java/soapService/dao/MongoConnection.java
+++ soapService/src/main/java/soapService/dao/MongoConnection.java
@@ -12,16 +12,21 @@ import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.bson.types.ObjectId;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoConnection {
 
-	private String mongo_url = "mongodb+srv://admin:passer@cluster0.k8qnj.mongodb.net/Blog?retryWrites=true&w=majority";
 	public MongoDatabase database;
 
 	public MongoConnection() {
-		ConnectionString connectionString = new ConnectionString(this.mongo_url);
+		ConnectionString connectionString = new ConnectionString(this.getConfig());
 		MongoClientSettings settings = MongoClientSettings.builder()
 				.applyConnectionString(connectionString)
 				.build();
@@ -50,12 +55,30 @@ public class MongoConnection {
 				.deleteOne(new Document("_id", new ObjectId(id)));
 	}
 
-	public static void main(String[] args) {
-		MongoConnection connection = new MongoConnection();
-		FindIterable<Document> result = connection.findDocument("users", "{name: 'admin'}");
-		result.forEach(document -> System.out.println(document.toJson()));
+	public String getConfig() {
+		String mongodb_url = "";
+		try {
+			String configPath = "src/application.properties"; //TODO: pass env in as parameter
+			System.out.println(configPath);
+			final File configFile = new File(configPath);
+			FileInputStream input = new FileInputStream(configFile);
+
+			Properties prop = new Properties();
+			prop.load(input);
+			mongodb_url = prop.getProperty("MONGODB_URL");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return  mongodb_url;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//MongoConnection connection = new MongoConnection();
+		//FindIterable<Document> result = connection.findDocument("users", "{name: 'admin'}");
+		//result.forEach(document -> System.out.println(document.toJson()));
 		//connection.insertDocument("users", "{ name: 'warner', login: 'warner', password: 'warner' }");
 		//connection.deleteDocument("users","6184ffe860f2946896c46d0b");
+		//System.out.println(@Value("${MONGODB_URL}"));
 	}
 
 
