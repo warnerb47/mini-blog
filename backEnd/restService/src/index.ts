efbb5ae15850475ca902fff891a04c6c71b56c7a
warnerb47 <warnerbang@gmail.com>
2021-11-18 12:44:43 +0000

    config: change .env path

--- backEnd/restService/src/index.ts
+++ backEnd/restService/src/index.ts
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+    path: "C:/env/mini-blog/backEnd/restService/.env"
+});
+// dotenv.config();
 import { startServer } from './rest';
 import { connectDB } from './config/mongoConfig';
 
